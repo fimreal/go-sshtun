c4488e48a5eb5cc676c3ba1b233504dff8a49ea0
fimreal <6913651@qq.com>
2023-07-19 10:19:20 +0800

    update default port 10080 =>1080, because port 10080 is unsafe in windows system

--- app/flags.go
+++ app/flags.go
@@ -13,7 +13,7 @@ func init() {
 	pflag.BoolP("debug", "d", false, "debug mode")
 	pflag.BoolP("sysproxy", "s", false, "enable system proxy")
 	pflag.StringP("pac", "P", "", "enable pac. need a pac url, like \"http://127.0.0.1:8000/my.pac\", or use embedded rules(gfw, tiny)")
-	pflag.StringP("listen", "l", "127.0.0.1:10080", "service listing on")
+	pflag.StringP("listen", "l", "127.0.0.1:1080", "service listing on")
 
 	// ssh
 	pflag.StringP("host", "h", "", "ssh server address, like \"epurs.com:2222\"")
